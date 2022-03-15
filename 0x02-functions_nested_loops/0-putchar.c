2534637fc07ab249869e6358fc145c8a7f34eb4b
SEVERIN <geekseverin@gmail.com>
2022-03-15 13:58:23 -0700

    0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,31 @@
 #include "main.h"
+
 /**
- *main - entry point.
- *Description: program starts and ends here.
- *Return: 0 on success.
+ * main - Entry block
+ * Description: prints holberton on new line
+ * Return: 0
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-char text[9] = "Holberton";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(text[i]);
-}
-_putchar('\n');
-return (0);
+	char c[9] = "_putchar";
+
+	char text[9] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+		i++;
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
