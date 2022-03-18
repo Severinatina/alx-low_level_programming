0dd1eb85bba333c0bb6e830f269066ff74a105bf
SEVERIN <geekseverin@gmail.com>
2022-03-18 15:18:10 -0700

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,12 @@
-# ifndef _main_h_
-# définir  _main_h_
-
-int  _putchar ( char c);
-int  _isupper ( int c);
- entier _isdigit ( entier c );
-int  mul ( int a, int b);
-void  print_numbers ( void );
-void  print_most_numbers ( void );
-void  more_numbers ( void );
-void  print_number ( int n);
-void  print_line ( int n);
-void  print_diagonal ( int n);
-void  print_square ( taille int );
-void  print_triangle ( taille int );
-
-# endif
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
