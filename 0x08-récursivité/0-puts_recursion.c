6a41f35ef7899bd4793302ff8b8a436b25668034
Severinatina <99341840+Severinatina@users.noreply.github.com>
2022-04-01 22:55:43 +0200

    Update 0-puts_recursion.c

--- 0x08-récursivité/0-puts_recursion.c
+++ 0x08-récursivité/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to string
+ * *Return: void
  */
-void _puts_recursion(char *s)
+
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s > '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
